fbdc5a8e06d8189e68acdc8faf6121862064ebe7
kenzodiac <kennethfujimura@gmail.com>
2023-03-15 00:44:37 -0700

    readme edit

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 - Last Revised: 3/15/2023
 - "Advice Generator In React"
 - This is a project in React to practice our react skills. In it, we pulled 'advice' from an API, and have that advice appear on screen via a button click. We had to tightly follow a pre-designed prototype and follow a style guide.
-- Peer Reviewed By: 
+- Peer Reviewed By: Amardeep Mann - ""
+- Github Repo: https://github.com/kenzodiac/advicegenerator
+- Azure Deployment: https://platitudegenerator.azurewebsites.net/
--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+// - Kenneth Fujimura
+// - Last Revised: 3/15/2023
+// - "Advice Generator In React"
+// - This is a project in React to practice our react skills. In it, we pulled 'advice' from an API, and have that advice appear on screen via a button click. We had to tightly follow a pre-designed prototype and follow a style guide.
+// - Peer Reviewed By: Amardeep Mann - ""
+// - Github Repo: https://github.com/kenzodiac/advicegenerator
+// - Azure Deployment: https://platitudegenerator.azurewebsites.net/
+
 import MainPgComponent from './Components/MainPgComponent';
 import './App.css'
-
 
 function App() {
   return (
